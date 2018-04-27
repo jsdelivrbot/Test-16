aab3e79a53cc8197ad7b20a148b48c78c6d832ae
Sairam Vinnakota <sai070cm118@users.noreply.github.com>
2018-04-27 15:34:41 +0530

    gtproxy added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var gtproxy=require('gtproxyservice')
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
